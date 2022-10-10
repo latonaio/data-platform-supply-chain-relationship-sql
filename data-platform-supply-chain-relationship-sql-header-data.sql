ee9cc133a5036f7bc671e96fcb7cf78a2b9d8e51
Yurino Oga <yurino.o@latona.io>
2022-10-10 13:37:39 +0900

    release

--- data-platform-supply-chain-relationship-sql-header-data.sql
+++ data-platform-supply-chain-relationship-sql-header-data.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `data_platform_supply_chain_realtionship_header_data`
 (
-    `BusinessPartner`               varchar(10) NOT NULL,
-    `RelationshipCounter`           varchar(4) NOT NULL,
-    `RelationshipBusinessPartner`   varchar(10) DEFAULT NULL,
+    `BusinessPartner`               int(10) NOT NULL,
+    `RelationshipCounter`           int(4) NOT NULL,
+    `RelationshipBusinessPartner`   int(10) DEFAULT NULL,
     `RelationshipType`              varchar(2) DEFAULT NULL,
     PRIMARY KEY (`BusinessPartner`, `RelationshipCounter`),
 ) ENGINE = InnoDB
